ca5942c3c030b304bccfa3320ec3c33ac6bdb492
dinads <90200168+dinads@users.noreply.github.com>
2021-09-15 15:42:02 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.py
@@ -0,0 +1,4 @@
+basic.show_string("LOVE METER")
+def on_pin_pressed_p0():
+   pass
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.showString("LOVE METER")
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    
+})
